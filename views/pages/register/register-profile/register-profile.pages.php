400025b40e49237042fe8d1f28d7fc88b5899fb4
Emillie <emillienicole.duarte@gmail.com>
2022-04-20 16:45:27 -0300

    Tela de escolha de perfil (Aluno ou Empresa)

---
+++ views/pages/register/register-profile/register-profile.pages.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../../styles/style.global.css">
+        <link rel="stylesheet" href="../../../styles/fonts.style.css">
+        <link rel="stylesheet" href="../../../styles/colors.style.css">
+        <link rel="stylesheet" href="../../../styles/font-format.style.css">
+        <link rel="shortcut icon" href="../../../images/favicon/favicon-16x16.png" type="image/x-icon">
+        <link rel="stylesheet" href="register-profile.pages.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>Cadastro - HEELP!</title>
+    </head>
+    <body>
+        <div class="body-container background-color">
+            <div class="container">
+                <a href="#"><img src="../../../images/components/arrow-back.svg" alt="Botão de voltar" class="back-button"></a>
+                <div class="page-content">
+                    <img src="../../../images/logo/logo-and-name.svg" alt="Logo do Help" class="logo-help">
+                    <p class="gray-title display text-center title-o-que">O que você é?</p>
+                    <div class="container-fluid">
+                        <div class="card-profile-container d-flex justify-content-center">
+                            <div class="card-container row">
+                                <div class="d-flex justify-content-lg-between justify-content-md-between justify-content-sm-center justify-content-center col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-4 mb-sm-4 mb-md-4 gx-md-5 gx-lg-5 gx-xl-5">
+                                    <a href="#">
+                                        <div class="card-profile gray-card card-profile-aluno">
+                                            <div class="card-content">
+                                                <img src="../../../images/profiles/profile-aluno.svg" alt="Avatar do aluno" class="avt-pic">
+                                                <p class="white-title title1 title-profile text-center card-text">Aluno</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="d-flex justify-content-lg-center justify-content-md-center justify-content-sm-center justify-content-center col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                                    <a href="#" style="text-decoration: none;">
+                                        <div class="card-profile gray-card">
+                                            <div class="card-content">
+                                                <img src="../../../images/profiles/profile-empresa-parceira.svg" alt="Avatar da empresa parceira" class="avt-pic">
+                                            </div>
+                                            <p class="white-title title1 title-profile card-text text-center">Empresa Parceira</p>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
