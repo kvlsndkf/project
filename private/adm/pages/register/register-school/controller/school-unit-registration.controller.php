de4241be13673f170d3636e11afd49a9512239b3
Helena <hmiranda1818@gmail.com>
2022-05-04 10:10:48 -0300

    feat: list, register and methods for school

---
+++ private/adm/pages/register/register-school/controller/school-unit-registration.controller.php
@@ -0,0 +1,69 @@
+<?php
+include_once('/xampp/htdocs' . '/project/database/connection.php');
+require_once('/xampp/htdocs' . '/project/classes/schools/School.class.php');
+require_once('/xampp/htdocs' . '/project/classes/schools/Teacher.class.php');
+
+session_start();
+
+if (isset($_POST['register'])) {
+    if (!isset($_FILES['photo'])) {
+        $school = new School();
+
+        $school->setName($_POST['name']);
+        $school->setAddress(($_POST['address']));
+        $school->setHaveAccount(isset($_POST['createAccount']));
+
+        $school->registerSchool($school);
+    }
+
+
+    if (isset($_POST['createAccount']) && isset($_FILES['photo'])) {
+        $schoolPhoto = $_FILES['photo'];
+        $namePhoto = $schoolPhoto['name'];
+        $nameId = uniqid();
+        $extensionPhoto = strtolower(pathinfo($namePhoto, PATHINFO_EXTENSION));
+
+        if ($schoolPhoto['size'] > 2097152) {
+            $_SESSION['statusAlert'] = "Arquivo muito grande. Máximo: 2MB.";
+            header('Location: /project/private/adm/pages/register/register-school/form-register-school.page.php');
+        }
+
+        if ($schoolPhoto['error']) {
+            $_SESSION['statusNegative'] = "Falha ao enviar arquivo.";
+            header('Location: /project/private/adm/pages/register/register-school/form-register-school.page.php');
+        }
+
+        if ($extensionPhoto != "jpg" && $extensionPhoto != "jpeg" && $extensionPhoto != "png") {
+            $_SESSION['statusAlert'] = "Tipo de arquivo incorreto. Permitidos: jpg, jpeg e png.";
+            header('Location: /project/private/adm/pages/register/register-school/form-register-school.page.php');
+        }
+
+        if (is_null($_SESSION['statusAlert']) && is_null($_SESSION['statusNegative'])) {
+            $pathToSave = "/project/private/adm/pages/register/upload/schools/" . $nameId . "." . $extensionPhoto;
+            $path = "/xampp/htdocs/project/private/adm/pages/register/upload/schools/" . $nameId . "." . $extensionPhoto;
+            $success = move_uploaded_file($schoolPhoto["tmp_name"], $path);
+
+            if ($success) {
+                $teacher = new Teacher();
+                $teacher = $_POST['idTeachers'];
+
+                $school = new School();
+                $school->setName($_POST['name']);
+                $school->setAddress(($_POST['address']));
+                $school->setHaveAccount(($_POST['createAccount']));
+                $school->setAbout($_POST['about']);
+                $school->setGithub($_POST['github']);
+                $school->setLinkedin($_POST['linkedin']);
+                $school->setFacebook($_POST['facebook']);
+                $school->setInstagram($_POST['instagram']);
+                $school->setPhoto($pathToSave);
+
+                $school->setTeacher($teacher);
+                $school->registerSchool($school);
+            } else {
+                $_SESSION['statusNegative'] = "Falha ao carregar a imagem, tente novamente...";
+                header('Location: /project/private/adm/pages/register/register-school/form-register-school.page.php');
+            }
+        }
+    }
+}
---
+++ private/adm/pages/register/register-school/list-school.page.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Etec's | Heelp!</title>
+
+    <!-- CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Mensagem de sucesso ⬇️ -->
+    <?php if (isset($_SESSION['statusPositive']) && $_SESSION != '') { ?>
+
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+            </symbol>
+        </svg>
+
+        <div class="alert alert-success d-flex align-items-center alert-dismissible fade show" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
+                <use xlink:href="#check-circle-fill" />
+            </svg>
+            <div>
+                <strong>Tudo certo!</strong>
+                <?php echo $_SESSION['statusPositive']; ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+    <?php unset($_SESSION['statusPositive']);
+    } ?>
+
+    <!-- Cadastro escola ⬇️ -->
+    <a href="./form-register-school.page.php">
+        <div class="bg-primary text-white p-5">
+            Cadastro etec
+        </div>
+    </a>
+
+    <!-- JS Bootstrap ⬇️ -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
