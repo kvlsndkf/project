2765b4fcc412892c53c31dc7bc5d2652c32d4f8a
Helena <hmiranda1818@gmail.com>
2022-05-20 22:38:59 -0300

    feat: add textarea countdown

---
+++ private/adm/pages/js/textarea.js
@@ -0,0 +1,37 @@
+const textarea = document.getElementById('about');
+const div = document.getElementById('contentTextArea');
+const counter = document.getElementById('counterTextArea');
+
+function colorDiv() {
+    div.classList.add("border-div");
+};
+
+function count(e) {
+    const inputLength = textarea.value.length;
+    const maxChars = 250;
+
+    counter.innerText = maxChars - inputLength;
+
+    if (inputLength >= maxChars) {
+        e.preventDefault();
+        div.classList.add("border-div-error");
+
+        textarea.setAttribute('readonly', 'true');
+
+        if (e.key === 'Backspace') {
+            textarea.removeAttribute('readonly');
+            textarea.value = textarea.value.substring(0, inputLength - 1);
+        }
+    } else {
+        div.classList.remove("border-div-error");
+    }
+
+};
+
+textarea.addEventListener("keyup", function (e) {
+    count(e);
+});
+
+(function (e) {
+    count(e);
+}());
