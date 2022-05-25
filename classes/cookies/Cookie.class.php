a47bb500c82b30df113849979f7a335c81f7bfef
Helena <hmiranda1818@gmail.com>
2022-05-25 11:49:31 -0300

    feat: cookie class saving and reading cookies

--- classes/cookies/Cookie.class.php
+++ classes/cookies/Cookie.class.php
@@ -4,22 +4,19 @@ class Cookie
 {
     private static $storage = array();
 
-    public static function reader( $key )
+    public static function reader($key)
     {
-        if( isset( $_COOKIE[ $key ] ) )
-        {
-            return $_COOKIE[ $key ];
-        }
-        else
-        {
-            if( isset( static::$storage[$key] ) )
-            return static::$storage[$key];
+        if (isset($_COOKIE[$key])) {
+            return $_COOKIE[$key];
+        } else {
+            if (isset(static::$storage[$key]))
+                return static::$storage[$key];
         }
     }
 
-    public static function writer( $key, $value)
+    public static function writer($key, $value)
     {
         static::$storage[$key] = $value;
-        setcookie( $key , $value , time() + (3600), '/');
+        setcookie($key, $value, time() + (3600), '/');
     }
 }
