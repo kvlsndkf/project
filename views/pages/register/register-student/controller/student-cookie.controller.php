8592e9129c19b5daaf6aabfad150f53e3c7ff45b
Helena <hmiranda1818@gmail.com>
2022-05-25 11:53:45 -0300

    feat: controller to save cookies

--- views/pages/register/register-student/controller/student-cookie.controller.php
+++ views/pages/register/register-student/controller/student-cookie.controller.php
@@ -20,6 +20,11 @@ if(isset($_POST['step1'])){
 if(isset($_POST['step2'])){
     $firstName = $_POST['firstName'];
     $surname = $_POST['surname'];
+
+    $nameSchool = $_POST['nameSchool'];
+    $nameCourse = $_POST['nameCourse'];
+    $nameModule = $_POST['nameModule'];
+
     $linkedin = $_POST['linkedin'];
     $github = $_POST['github'];
     $facebook = $_POST['facebook'];
@@ -27,6 +32,11 @@ if(isset($_POST['step2'])){
     
     Cookie::writer('firstName', $firstName);
     Cookie::writer('surname', $surname);
+
+    Cookie::writer('nameSchool', $nameSchool);
+    Cookie::writer('nameCourse', $nameCourse);
+    Cookie::writer('nameModule', $nameModule);
+
     isset($_POST['linkedin']) ? Cookie::writer('linkedin', $linkedin) : '';
     isset($_POST['github']) ? Cookie::writer('github', $github) : '';
     isset($_POST['facebook']) ? Cookie::writer('facebook', $facebook) : '';
