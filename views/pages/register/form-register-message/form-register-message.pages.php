9e7322c76183108e9fc95a670e480ac7832e4f5e
Arhur <arthuzito690@gmail.com>
2022-05-26 23:04:49 -0300

    atualizado o maxlength

--- views/pages/register/form-register-message/form-register-message.pages.php
+++ views/pages/register/form-register-message/form-register-message.pages.php
@@ -11,21 +11,28 @@
         <link rel="stylesheet" href="../../../../views/styles/fonts.style.css">
         <link rel="stylesheet" href="../../../../views/styles/colors.style.css">
         <link rel="shortcut icon" href="../../../../views/images/favicon/favicon-16x16.png" type="image/x-icon">
+        <link rel="stylesheet" href="../../../../style/style.css">
         <!-- CSS Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        
   <title>Fale Conosco - HEELP!</title>
 </head>
 <body>
+
+
+
 <form class="row g-3 needs-validation" action="./controller/message-unit-registration.controller.php" method="POST" enctype="multipart/form-data" novalidate>
     <div class="mb-3">
       <label for="exampleFormControlInput1" class="form-label">Deixe um email para entrarmos em contato</label>
       <input name="contact" type="email" class="form-control" id="exampleFormControlInput1" required placeholder="contato@email.com" autocomplete="off">
     </div>
-    <div id="contentTextArea" class="mb-3">
-      <label for="exampleFormControlTextarea1" class="form-label">Deixe sua crítica/avaliação</label>
-      <textarea name="message" id="about"class="form-control" id="exampleFormControlTextarea1" cols="30" rows="10"  placeholder="Nos conte como foi/está sendo a sua experiência" required ></textarea>
+    <div  class="mb-3">
+    
+    <label for="exampleFormControlTextarea1" class="form-label">Deixe sua crítica/avaliação</label>
+
+      <div id="contentTextArea"><textarea name="message" id="about" class="form-control" cols="30" rows="10"  placeholder="Nos conte como foi/está sendo a sua experiência" onclick="colorDiv();" required  maxlength="250"></textarea>
       <div><span id="counterTextArea">250</span></div>
-      
+      </div>
     </div>
     <div class="col-12">
     <input class="btn btn-primary" type="submit" value="Cadastrar"  name="register" onclick = "GFG_Fun()"></input>
@@ -61,6 +68,7 @@
 
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
 
 
     <!-- JS Visibility Inputs -->
