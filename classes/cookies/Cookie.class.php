eab1131cee3999d4c7e5e85832d77de2519ef988
Helena <hmiranda1818@gmail.com>
2022-05-27 21:49:19 -0300

    feat: description of methods and new method to delete cookie

--- classes/cookies/Cookie.class.php
+++ classes/cookies/Cookie.class.php
@@ -2,8 +2,13 @@
 
 class Cookie
 {
+    //attribute
     private static $storage = array();
 
+    /**
+     * @method reader() save cookies by 
+     * @param string $key 
+     */
     public static function reader($key)
     {
         if (isset($_COOKIE[$key])) {
@@ -14,9 +19,23 @@ class Cookie
         }
     }
 
+    /**
+     * @method reader() read cookies by 
+     * @param string $key 
+     * @param string $value 
+     */
     public static function writer($key, $value)
     {
         static::$storage[$key] = $value;
         setcookie($key, $value, time() + (3600), '/');
+    }
+
+    /**
+     * @method reader() delete cookies by 
+     * @param string $key 
+     */
+    public static function delete($key)
+    {
+        setcookie($key, '', time() - (3600), '/');
     }
 }
