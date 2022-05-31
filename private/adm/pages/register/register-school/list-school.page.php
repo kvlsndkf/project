5acea3cdd1a8067ac78fb542e1419edbe6024f9d
MayaraPereira17 <mayaracapereira45@gmail.com>
2022-05-30 22:35:17 -0300

    correçao da linkagem direcionada para o dashboard

--- private/adm/pages/register/register-school/list-school.page.php
+++ private/adm/pages/register/register-school/list-school.page.php
@@ -112,7 +112,7 @@ try {
                 </li>
 
                 <li class="sidebar-li">
-                    <a href="#" class="sidebar-a-items">
+                    <a href="../../dashboard/dashboard.page.php" class="sidebar-a-items">
                         <img class="sidebar-img" src="../../../../../views/images/components/dashboard-img.svg" alt="">
                         <p class="sidebar-option normal-18-bold-title-2">Dashboard</p>
                     </a>
