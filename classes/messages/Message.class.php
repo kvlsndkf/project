be673fa3d6e7da3b29fc7b29a59b40abd654bad7
Giovanna <giovannasales0806@gmail.com>
2022-06-03 20:38:54 -0300

    Listar as mensagens

--- classes/messages/Message.class.php
+++ classes/messages/Message.class.php
@@ -106,7 +106,133 @@ class Message
             
         }
     }
+
+     /**
+     * @method listMessage() lists the messages by 
+     * @param string $search 
+     */
+
+    public function listMessage(string $search = '', string $filter = ''): array | false
+    {
+        $connection = Connection::connection();
+
+        try {
+            if (!is_null($search) && !empty($search)) {
+                $result = $this->searchMessage($search);
+                return $this->buildMessageList($result);
+            }
+
+            if (!empty($filter)) {
+                $result = $this->filterMessage($filter);
+                return $this->buildMessageList($result);
+            }
+
+
+            $stmt = $connection->prepare("SELECT * FROM messages");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $this->buildMessageList($result);
+    }
     
+     catch (Exception $e) {
+        echo $e->getMessage();
+    }
+
+  }
+
+   //----------------------------
+    /**
+     * @method buildMessageList() organize the list of teachers by 
+     * @param array $result 
+     */
+    private function buildMessageList(array | false $result)
+    {
+        $messages = [];
+
+        for ($i = 0; $i < count($result); $i++) {
+            $row = $result[$i];
+            $message = new Message();
+            $message->id = $row['id'];
+            $message->contact = $row['contact'];
+            $message->status = $row['status'];
+            $message->message = $row['message'];
+            array_push($messages, $message);
+        }
+
+        $this->setResultBuildList($messages);
+        return $messages;
+    }
+
     
-    
+       //----------------------------
+    /**
+     * @method countCourses() count the teachers by 
+     * @param string $search 
+     */
+    public function countMessages(string $search = '')
+    {
+        $searching = (!is_null($search) && !empty($search));
+
+        if ($searching) {
+            $resultBuildList = $this->getResultBuildList();
+            $totalSearch = count($resultBuildList);
+            return "Resultado da pesquisa " . $totalSearch;
+        }
+
+        $connection = Connection::connection();
+        try {
+            $stmt = $connection->prepare("SELECT COUNT(id) AS total FROM messages");
+            $stmt->execute();
+
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return "Total (" . $result[0]['total']. ")";
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    //----------------------------
+    /**
+     * @method listMessageOfSearchBar() list messages for search bar  
+     */
+    public function listMessagesOfSearchBar()
+    {
+        $connection = Connection::connection();
+
+        try {
+            $stmt = $connection->prepare("SELECT * FROM messages ORDER BY message");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $this->buildMessageList($result);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    //----------------------------
+    /**
+     * @method searchMessage() search messages by 
+     * @param string $search 
+     */
+    private function searchMessage(string $search)
+    {
+        $connection = Connection::connection();
+
+        try {
+            $stmt = $connection->prepare("SELECT * FROM messages WHERE message LIKE '%$search%' ORDER BY message");
+
+            $stmt->execute();
+
+            $lines = $stmt->rowCount();
+
+            if ($lines == 00) {
+                $_SESSION['statusNegative'] = "Não existe registros.";
+            }
+
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
 }
