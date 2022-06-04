f658354fc91e390ff0659496cdde491d1f2a5c56
Helena <hmiranda1818@gmail.com>
2022-06-04 10:53:50 -0300

    feat: controller for read messages

---
+++ private/adm/pages/message/controller/read-message.controller.php
@@ -0,0 +1,12 @@
+<?php
+include_once('/xampp/htdocs' . '/project/database/connection.php');
+require_once('/xampp/htdocs' . '/project/classes/messages/Message.class.php');
+
+if (isset($_POST['readMessage'])) {
+    $message = new Message();
+
+    $id = $_GET['messageID'];
+
+    $message->setStatus(false);
+    $message->readingTheMessage($message, $id);
+}
