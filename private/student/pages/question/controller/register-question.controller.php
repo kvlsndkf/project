673a0d5d6390e5b23f08a63a935bf3f294ab9875
Helena <hmiranda1818@gmail.com>
2022-06-04 22:46:24 -0300

    feat: register question

--- private/student/pages/question/controller/register-question.controller.php
+++ private/student/pages/question/controller/register-question.controller.php
@@ -15,9 +15,9 @@ if (isset($_POST['question'])) {
     $category = new Category();
     $category = $_POST['category'];
 
-    $question = new Question();
+    $xp = $category == 3 ? 100 : 300;
 
-    $xp = $category === "Apoio" ? 100 : 300;
+    $question = new Question();
 
     $textQuestion = $_POST['textQuestion'];
 
@@ -28,22 +28,93 @@ if (isset($_POST['question'])) {
 
     if (!isset($course)) {
         $_SESSION['statusNegative'] = "Selecione um curso para continuar.";
-        return header('Location: /project/views/pages/question/question.page.php');
+        return header('Location: /project/private/student/pages/question/question.page.php');
     }
 
     if (!isset($subject)) {
         $_SESSION['statusNegative'] = "Selecione uma matéria para continuar.";
-        return header('Location: /project/views/pages/question/question.page.php');
+        return header('Location: /project/private/student/pages/question/question.page.php');
     }
 
     if (!isset($category)) {
         $_SESSION['statusNegative'] = "Selecione uma categoria para continuar.";
-        return header('Location: /project/views/pages/question/question.page.php');
+        return header('Location: /project/private/student/pages/question/question.page.php');
     }
 
     if (!isset($textQuestion) || empty($textQuestion)) {
         $_SESSION['statusNegative'] = "Escreva uma pergunta para continuar.";
         return header('Location: /project/private/student/pages/question/question.page.php');
+    }
+
+    if (!empty($_FILES['photo']['name'])) {
+        echo "oi p";
+        $questionPhoto = $_FILES['photo'];
+        $namePhoto = $questionPhoto['name'];
+        $nameId = uniqid();
+        $extensionPhoto = strtolower(pathinfo($namePhoto, PATHINFO_EXTENSION));
+
+        if ($questionPhoto['size'] > 2097152) {
+            $_SESSION['statusAlert'] = "Arquivo muito grande. Máximo: 2MB.";
+            return header('Location: /project/private/student/pages/question/question.page.php');
+        }
+
+        if ($questionPhoto['error']) {
+            $_SESSION['statusNegative'] = "Falha ao enviar arquivo.";
+            return header('Location: /project/private/student/pages/question/question.page.php');
+        }
+
+        if ($extensionPhoto != "jpg" && $extensionPhoto != "jpeg" && $extensionPhoto != "png") {
+            $_SESSION['statusAlert'] = "Tipo de arquivo incorreto. Permitidos: jpg, jpeg e png.";
+            return header('Location: /project/private/student/pages/question/question.page.php');
+        }
+
+        if (!isset($_SESSION['statusAlert']) && !isset($_SESSION['statusNegative'])) {
+            $pathToSave = "/project/private/student/pages/question/upload/images/" . $nameId . "." . $extensionPhoto;
+            $path = "/xampp/htdocs/project/private/student/pages/question/upload/images/" . $nameId . "." . $extensionPhoto;
+            $success = move_uploaded_file($questionPhoto["tmp_name"], $path);
+
+            if ($success) {
+                $question->setPhoto($pathToSave);
+            } else {
+                $_SESSION['statusNegative'] = "Falha ao carregar a imagem, tente novamente...";
+                return header('Location: /project/private/student/pages/question/question.page.php');
+            }
+        }
+    }
+
+    if (!empty($_FILES['document']['name'])) {
+        $questionDocument = $_FILES['document'];
+        $nameDocument = $questionDocument['name'];
+        $question->setNameDocument($nameDocument);
+        $nameId = uniqid();
+        $extensionDocument = strtolower(pathinfo($nameDocument, PATHINFO_EXTENSION));
+
+        if ($questionDocument['size'] > 2097152) {
+            $_SESSION['statusAlert'] = "Arquivo muito grande. Máximo: 2MB.";
+            return header('Location: /project/private/student/pages/question/question.page.php');
+        }
+
+        if ($questionDocument['error']) {
+            $_SESSION['statusNegative'] = "Falha ao enviar documento.";
+            return header('Location: /project/private/student/pages/question/question.page.php');
+        }
+
+        if ($extensionDocument != "pdf" && $extensionDocument != "txt" && $extensionDocument != "doc" && $extensionDocument != "ppt" && $extensionDocument != "xml") {
+            $_SESSION['statusAlert'] = "Tipo de arquivo incorreto. Permitidos: pdf, txt, doc, xml, e ppt.";
+            return header('Location: /project/private/student/pages/question/question.page.php');
+        }
+        if (is_null($_SESSION['statusAlert']) && is_null($_SESSION['statusNegative'])) {
+            $pathToSave = "/project/private/student/pages/question/upload/docs/" . $nameId . "." . $extensionDocument;
+            $path = "/xampp/htdocs/project/private/student/pages/question/upload/docs/" . $nameId . "." . $extensionDocument;
+            $success = move_uploaded_file($questionDocument["tmp_name"], $path);
+            
+            if ($success) {
+                $question->setDocument($pathToSave);
+            } else {
+                $_SESSION['statusNegative'] = "Falha ao carregar a imagem, tente novamente...";
+                return header('Location: /project/private/student/pages/question/question.page.php');
+            }
+        }
     }
 
     $question->setXp($xp);
--- private/student/pages/question/question.page.php
+++ private/student/pages/question/question.page.php
@@ -57,13 +57,35 @@ try {
     <?php unset($_SESSION['statusNegative']);
     } ?>
 
+    <!-- Mensagem de alerta ⬇️ -->
+    <?php if (isset($_SESSION['statusAlert']) && $_SESSION != '') { ?>
+
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+            </symbol>
+        </svg>
+
+        <div class="alert alert-warning d-flex align-items-center  alert-dismissible fade show" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:">
+                <use xlink:href="#exclamation-triangle-fill" />
+            </svg>
+            <div>
+                <strong>Ops...</strong>
+                <?php echo $_SESSION['statusAlert']; ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+    <?php unset($_SESSION['statusAlert']);
+    } ?>
+
     <label for="">Fazer pegunta</label>
 
     <br>
     <br>
     <form action="./controller/register-question.controller.php" method="post" enctype="multipart/form-data">
         <label for="">Curso</label>
-        <select name="course" id="selectCourse" class="selectCourse w-100">
+        <select name="course" id="selectCourse" class="selectCourse w-100" onchange="courseValue()">
             <optgroup label="Seu curso">
                 <?php for ($i = 0; $i < count($listCourseOfStudent); $i++) {
                     $row = $listCourseOfStudent[$i] ?>
@@ -111,8 +133,9 @@ try {
         <hr>
 
         <label for="">Adicionar fotos do seu computador</label>
+        <img src="" alt="" id="imageFile">
         <br>
-        <input type="file" name="photo">
+        <input type="file" name="photo" onchange="previewImage(this)">
 
         <br>
         <br>
@@ -146,6 +169,9 @@ try {
 
     <!-- Include the Quill library -->
     <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+
+    <!-- JS Preview Image ⬇️ -->
+    <script src="../../js/preview-image.js"></script>
 
     <script>
         $(".selectCourse").select2({
@@ -184,7 +210,7 @@ try {
             }, {
                 'script': 'sub'
             }],
-            ['link', 'image', 'video']
+            ['link', 'video']
         ]
 
         var editor = new Quill('#editor', {
@@ -234,6 +260,24 @@ try {
         (async function() {
             await courseValue();
         }());
+    </script>
+
+    <script>
+        function previewImage(self){
+            const imageFile = document.getElementById("imageFile");
+            const file = self && self.files[0];
+
+            if(!file){
+                imageFile.style.display = "none";
+                return;
+            }
+
+            if(file){
+                imageFile.style.display = "block";
+                imageFile.src = URL.createObjectURL(file);
+                return;
+            }
+        }
     </script>
 </body>
 
