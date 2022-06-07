e6e379926a07ed26282936ab432fc6df569f93ac
Helena <hmiranda1818@gmail.com>
2022-06-07 00:05:10 -0300

    feat: reply, listing and rules

---
+++ classes/answers/Answer.class.php
@@ -0,0 +1,320 @@
+<?php
+include_once('/xampp/htdocs' . '/project/database/connection.php');
+require_once('/xampp/htdocs' . '/project/classes/questions/Question.class.php');
+
+class Answer
+{
+    //attributes
+    public int $id;
+    public int $xp;
+    public int $idQuestion;
+    public int $creatorAnswer;
+    public float $avaliation;
+    public int $likeAnswer;
+    public string $answer;
+    public string $createdAt;
+    public string $updatedAt;
+    public int $student;
+    public string $photo = "";
+    public string $document = "";
+    public string $nameDocument = "";
+
+    //getters and setters
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    //----------------------------
+    public function getXp()
+    {
+        return $this->xp;
+    }
+    public function setXp($xp)
+    {
+        $this->xp = $xp;
+    }
+    //----------------------------
+    public function getIdQuestion()
+    {
+        return $this->idQuestion;
+    }
+    public function setIdQuestion($idQuestion)
+    {
+        $this->idQuestion = $idQuestion;
+    }
+    //----------------------------
+    public function getCreatorAnswer()
+    {
+        return $this->creatorAnswer;
+    }
+    public function setCreatorAnswer($creatorAnswer)
+    {
+        $this->creatorAnswer = $creatorAnswer;
+    }
+    //----------------------------
+    public function getAnswer()
+    {
+        return $this->answer;
+    }
+    public function setAnswer($answer)
+    {
+        $this->answer = $answer;
+    }
+    //----------------------------
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+    //----------------------------
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+    //----------------------------
+    public function getStudent()
+    {
+        return $this->student;
+    }
+    public function setStudent($student)
+    {
+        $this->student = $student;
+    }
+    //----------------------------
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+    }
+    //----------------------------
+    public function getDocument()
+    {
+        return $this->document;
+    }
+    public function setDocument($document)
+    {
+        $this->document = $document;
+    }
+    //----------------------------
+    public function getNameDocument()
+    {
+        return $this->nameDocument;
+    }
+    public function setNameDocument($nameDocument)
+    {
+        $this->nameDocument = $nameDocument;
+    }
+    //----------------------------
+    public function getAvaliation()
+    {
+        return $this->avaliation;
+    }
+    public function setAvaliation($avaliation)
+    {
+        $this->avaliation = $avaliation;
+    }
+    //----------------------------
+    public function getLikeAnswer()
+    {
+        return $this->likeAnswer;
+    }
+    public function setLikeAnswer($likeAnswer)
+    {
+        $this->likeAnswer = $likeAnswer;
+    }
+    //----------------------------
+    //methods
+
+    public function registerAnswer($answer)
+    {
+        $connection = Connection::connection();
+
+        //id creator question
+        $question = new Question();
+        $questionID = $this->getIdQuestion();
+
+        //creator question
+        $creatorQuestion = $question->getCreatorQuestionById($questionID);
+
+        //creator answer
+        $creatorAnswerID = $this->getCreatorAnswer();
+
+        //xp latest question
+        $xpQuestion = $question->getLatestXpQuestion($questionID);
+
+        //xp for creator answer
+        $xpAnswer = $creatorAnswerID == $creatorQuestion[0]['student_id'] ? 0 : $xpQuestion[0]['xp'];
+
+        try {
+            $stmt = $connection->prepare("INSERT INTO answers(answer, photo, document, document_name, avaliation, like_answer, question_id, answer_creator_id, created_at)
+                                         VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW())");
+
+            $stmt->bindValue(1, $answer->getAnswer());
+            $stmt->bindValue(2, $answer->getPhoto());
+            $stmt->bindValue(3, $answer->getDocument());
+            $stmt->bindValue(4, $answer->getNameDocument());
+            $stmt->bindValue(5, $answer->getAvaliation());
+            $stmt->bindValue(6, $answer->getLikeAnswer());
+            $stmt->bindValue(7, $answer->getIdQuestion());
+            $stmt->bindValue(8, $answer->getCreatorAnswer());
+            $stmt->execute();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
+        try {
+            //adding the xp
+            $student = new Student();
+            $latestXpStudent = $student->getLatestXpStudent($creatorAnswerID);
+            $totalXp = $xpAnswer + $latestXpStudent[0]['xp'];
+
+            $xpForTheAnswer = $connection->prepare("UPDATE students SET xp = ?
+                                                    WHERE id = '$creatorAnswerID'");
+
+            $xpForTheAnswer->bindValue(1, $totalXp);
+            $xpForTheAnswer->execute();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
+        try {
+            if ($xpQuestion[0]['xp'] == 0) {
+                $newXp = 0;
+            } else {
+                $newXp = $xpQuestion[0]['xp'] - 50;
+            }
+
+            $updateXpQuestion = $xpAnswer == 0 ? $xpQuestion[0]['xp'] : $newXp;
+
+            $xpForTheQuestion = $connection->prepare("UPDATE questions SET xp = ?
+                                                        WHERE id = '$questionID'");
+
+            $xpForTheQuestion->bindValue(1, $updateXpQuestion);
+            $xpForTheQuestion->execute();
+
+            $_SESSION['statusPositive'] = "Resposta feita.";
+            header('Location: /project/private/student/pages/detail-question/detail-question.page.php?idQuestion=' . $questionID);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    /**
+     * @method listSchool() lists the schools by 
+     * @param string $search 
+     */
+    public function listAnswer(int $idQuestion)
+    {
+        $connection = Connection::connection();
+
+        try {
+            $stmt = $connection->prepare("SELECT answ.id, usr.photo, stu.first_name, stu.surname, module.name AS 'module', 
+                                        school.name AS 'school', answ.answer, answ.photo AS 'imageAnswer', answ.avaliation,
+                                        answ.like_answer, answ.document, answ.document_name, answ.created_at FROM students stu
+                                            
+                                        INNER JOIN schoolshasstudents ss
+                                        ON stu.id = ss.student_id
+                                        INNER JOIN schools school
+                                        ON ss.school_id = school.id
+                                        INNER JOIN modules module
+                                        ON module.id = stu.module_id
+                                        INNER JOIN answers answ
+                                        ON stu.id = answ.answer_creator_id
+                                        INNER JOIN users usr
+                                        ON stu.user_id = usr.id
+                                        INNER JOIN questions quest
+                                        ON answ.question_id = quest.id
+                                        WHERE quest.id = '$idQuestion'
+                                        ORDER BY answ.created_at DESC
+                                        ");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $this->buildAnswerList($result);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    private function buildAnswerList($result)
+    {
+        $answers = [];
+
+        for ($i = 0; $i < count($result); $i++) {
+            $row = $result[$i];
+            $answer = $this->buildAnswer($row);
+
+            array_push($answers, $answer);
+        }
+        return $answers;
+    }
+
+    private function buildAnswer($row)
+    {
+        $answer = new Answer();
+        $answer->id = $row['id'];
+        $answer->photo = $row['photo'];
+        $answer->firstName = $row['first_name'];
+        $answer->surname = $row['surname'];
+        $answer->module = $row['module'];
+        $answer->school = $row['school'];
+        $answer->answer = $row['answer'];
+        $answer->avaliation = $row['avaliation'];
+        $answer->like = $row['like_answer'];
+        $answer->image = $row['imageAnswer'];
+        $answer->document = $row['document'];
+        $answer->documentName = $row['document_name'];
+        $answer->created = $this->countCreatedAnswer($row['created_at']);
+
+        return $answer;
+    }
+
+    private function countCreatedAnswer($dateAnswer)
+    {
+        date_default_timezone_set('America/Sao_Paulo');
+        $CurrentDateAndTime = date('y-m-d H:i:s', time());
+
+        $CurrentDate =  new DateTime($CurrentDateAndTime);
+        $AnswerDate =  new DateTime($dateAnswer);
+
+        $result = $AnswerDate->diff($CurrentDate);
+
+        if ($result->s < 60 && $result->i == 0) {
+            return "Há poucos segundos";
+        }
+
+        if ($result->i > 0 && $result->h == 0) {
+            return $result->i . " minutos";
+        }
+
+        if ($result->h > 0 && $result->d == 0) {
+            return $result->h . " horas";
+        }
+
+        if ($result->d < 30 && $result->m == 0) {
+            return $result->d . " dias";
+        }
+
+        if ($result->m < 12 && $result->y == 0) {
+            return $result->m . " meses";
+        }
+
+        if ($result->y > 0) {
+            return $result->y . " anos";
+        }
+    }
+}
---
+++ private/student/pages/answer-question/controller/answer-question.controller.php
@@ -0,0 +1,108 @@
+<?php
+require_once('/xampp/htdocs' . '/project/classes/questions/Question.class.php');
+require_once('/xampp/htdocs' . '/project/classes/answers/Answer.class.php');
+require_once('/xampp/htdocs' . '/project/classes/users/Student.class.php');
+
+
+
+if (isset($_POST['answer'])) {
+    $answer = new Answer();
+
+    //question
+    $questionID = $_GET['idQuestion'];
+
+    //id creator answer
+    $userID = $_SESSION['idUser'];
+    $student = new Student();
+    $creatorAnswerID = $student->getStudentByUserID($userID);
+
+    $textAnswer = $_POST['textAnswer'];
+
+
+    if (!isset($textAnswer) || empty($textAnswer)) {
+        $_SESSION['statusNegative'] = "Escreva uma resposta para continuar.";
+        return header('Location: /project/private/student/pages/answer-question/answer-question.page.php?idQuestion=' . $questionID);
+    }
+
+    //photo
+    if (!empty($_FILES['photo']['name'])) {
+
+        $answerPhoto = $_FILES['photo'];
+        $namePhoto = $answerPhoto['name'];
+        $nameId = uniqid();
+        $extensionPhoto = strtolower(pathinfo($namePhoto, PATHINFO_EXTENSION));
+
+        if ($answerPhoto['size'] > 2097152) {
+            $_SESSION['statusAlert'] = "Arquivo muito grande. Máximo: 2MB.";
+            return header('Location: /project/private/student/pages/answer-question/answer-question.page.php?idQuestion=' . $questionID);
+        }
+
+        if ($answerPhoto['error']) {
+            $_SESSION['statusNegative'] = "Falha ao enviar arquivo.";
+            return header('Location: /project/private/student/pages/answer-question/answer-question.page.php?idQuestion=' . $questionID);
+        }
+
+        if ($extensionPhoto != "jpg" && $extensionPhoto != "jpeg" && $extensionPhoto != "png") {
+            $_SESSION['statusAlert'] = "Tipo de arquivo incorreto. Permitidos: jpg, jpeg e png.";
+            return header('Location: /project/private/student/pages/answer-question/answer-question.page.php?idQuestion=' . $questionID);
+        }
+
+        if (!isset($_SESSION['statusAlert']) && !isset($_SESSION['statusNegative'])) {
+            $pathToSave = "/project/private/student/pages/answer-question/upload/images/" . $nameId . "." . $extensionPhoto;
+            $path = "/xampp/htdocs/project/private/student/pages/answer-question/upload/images/" . $nameId . "." . $extensionPhoto;
+            $success = move_uploaded_file($answerPhoto["tmp_name"], $path);
+
+            if ($success) {
+                $answer->setPhoto($pathToSave);
+            } else {
+                $_SESSION['statusNegative'] = "Falha ao carregar a imagem, tente novamente...";
+                return header('Location: /project/private/student/pages/answer-question/answer-question.page.php?idQuestion=' . $questionID);
+            }
+        }
+    }
+
+    //document
+    if (!empty($_FILES['document']['name'])) {
+
+        $answerDocument = $_FILES['document'];
+        $nameDocument = $answerDocument['name'];
+        $answer->setNameDocument($nameDocument);
+        $nameId = uniqid();
+        $extensionDocument = strtolower(pathinfo($nameDocument, PATHINFO_EXTENSION));
+
+        if ($answerDocument['size'] > 2097152) {
+            $_SESSION['statusAlert'] = "Arquivo muito grande. Máximo: 2MB.";
+            return header('Location: /project/private/student/pages/answer-question/answer-question.page.php?idQuestion=' . $questionID);
+        }
+
+        if ($answerDocument['error']) {
+            $_SESSION['statusNegative'] = "Falha ao enviar documento.";
+            return header('Location: /project/private/student/pages/answer-question/answer-question.page.php?idQuestion=' . $questionID);
+        }
+
+        if ($extensionDocument != "pdf" && $extensionDocument != "txt" && $extensionDocument != "doc" && $extensionDocument != "ppt" && $extensionDocument != "xml" && $extensionDocument != "xlsx") {
+            $_SESSION['statusAlert'] = "Tipo de arquivo incorreto. Permitidos: pdf, txt, doc, xml, xlsx e ppt.";
+            return header('Location: /project/private/student/pages/answer-question/answer-question.page.php?idQuestion=' . $questionID);
+        }
+        if (is_null($_SESSION['statusAlert']) && is_null($_SESSION['statusNegative'])) {
+            $pathToSave = "/project/private/student/pages/answer-question/upload/docs/" . $nameId . "." . $extensionDocument;
+            $path = "/xampp/htdocs/project/private/student/pages/answer-question/upload/docs/" . $nameId . "." . $extensionDocument;
+            $success = move_uploaded_file($answerDocument["tmp_name"], $path);
+
+            if ($success) {
+                $answer->setDocument($pathToSave);
+            } else {
+                $_SESSION['statusNegative'] = "Falha ao carregar a imagem, tente novamente...";
+                return header('Location: /project/private/student/pages/answer-question/answer-question.page.php?idQuestion=' . $questionID);
+            }
+        }
+    }
+
+    //answer
+    $answer->setIdQuestion($questionID);
+    $answer->setCreatorAnswer($creatorAnswerID[0]['id']);
+    $answer->setAnswer($textAnswer);
+    $answer->setAvaliation(0);
+    $answer->setLikeAnswer(0);
+    $answer->registerAnswer($answer);
+}
