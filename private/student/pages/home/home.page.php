def5200dcd80757b2adb611368823ad0058a7868
Tiago Alves <tchaguwu@gmail.com>
2022-06-15 17:04:25 -0300

    commit

--- private/student/pages/home/home.page.php
+++ private/student/pages/home/home.page.php
@@ -128,11 +128,11 @@ try {
                 </symbol>
             </svg>
 
-            <div class="alert alert-success d-flex align-items-center alert-dismissible fade show" role="alert">
-                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
+            <div class="alert alert-success d-flex align-items-center alert-dismissible div-alert fade show" role="alert">
+                <svg class="bi flex-shrink-0 me-2 div-alert" width="24" height="24" role="img" aria-label="Success:">
                     <use xlink:href="#check-circle-fill" />
                 </svg>
-                <div>
+                <div class="div-alert">
                     <strong>Tudo certo!</strong>
                     <?php echo $_SESSION['statusPositive']; ?>
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
