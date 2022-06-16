ff52493f67f33188aa8fbfaa5b0e85cccbf813ce
Tiago Alves <tchaguwu@gmail.com>
2022-06-16 17:00:04 -0300

    fix: start

--- private/student/pages/home/home.page.php
+++ private/student/pages/home/home.page.php
@@ -107,6 +107,13 @@ try {
 
             </div>
 
+            <!-- <div class="d-0">
+                <a href="" class="leftbar-bottom">
+                    <img src="../../../../views/images/components/profile-icon.svg" alt="">
+                    <p class="profile-name normal-16-bold-title-3">Cameron Murphy</p>
+                </a>
+                
+            </div> -->
 
         </nav>
         <div class="corpo-feed">
@@ -408,68 +415,6 @@ try {
         </nav>
     </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="modal-<?php echo $row->id; ?>" tabindex="-1" aria-labelledby="modalLabel-<?php echo $row->id; ?>" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="modalLabel-<?php echo $row->id; ?>">Relatar um problema</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Nos ajude a entender o problema, o que está acontecendo com esse post?
-
-                        <form action="../question/controller/denunciation-question.controller.php" method="post">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="denunciation" id="radio1-<?php echo $row->id; ?>" value="Não tenho interesse nesse post" required>
-                                <label class="form-check-label" for="radio1-<?php echo $row->id; ?>">
-                                    Não tenho interesse nesse post
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="denunciation" id="radio2-<?php echo $row->id; ?>" value="É suspeito ou está enviando span">
-                                <label class="form-check-label" for="radio2-<?php echo $row->id; ?>">
-                                    É suspeito ou está enviando span
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="denunciation" id="radio3-<?php echo $row->id; ?>" value="É abusivo ou nocivo">
-                                <label class="form-check-label" for="radio3-<?php echo $row->id; ?>">
-                                    É abusivo ou nocivo
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="denunciation" id="radio4-<?php echo $row->id; ?>" value="As informações são enganosas">
-                                <label class="form-check-label" for="radio4-<?php echo $row->id; ?>">
-                                    As informações são enganosas
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="denunciation" id="radio5-<?php echo $row->id; ?>" value="Manifesta intenções de automutilação ou suicídio">
-                                <label class="form-check-label" for="radio5-<?php echo $row->id; ?>">
-                                    Manifesta intenções de automutilação ou suicídio
-                                </label>
-                            </div>
-
-                            <div>
-                                <input type="hidden" name="post_link" id="" value="<?php echo $row->linkQuestion; ?>">
-                                <input type="hidden" name="createdBy" id="" value="<?php echo $idUser; ?>">
-                                <input type="hidden" name="denounciedId" id="" value="<?php echo $userCreatorQuestion[0]['user_id']; ?>">
-                                <input type="hidden" name="questionId" id="" value="<?php echo $row->id; ?>">
-                            </div>
-
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="submit" name="register" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <hr>
-    <?php } ?>
 
     <!-- JS JQuery ⬇️ -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
