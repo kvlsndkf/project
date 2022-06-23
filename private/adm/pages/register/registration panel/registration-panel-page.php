071035e3a9a5b622b3e5f931ca94f5ccda773de3
Emillie <emillienicole.duarte@gmail.com>
2022-06-23 01:37:23 -0300

    fix:all

--- private/adm/pages/register/registration panel/registration-panel-page.php
+++ private/adm/pages/register/registration panel/registration-panel-page.php
@@ -87,13 +87,6 @@ include_once('/xampp/htdocs' . '/project/private/validation/validation-administr
                     </li>
 
                     <li class="sidebar-li">
-                        <a href="#" class="sidebar-a">
-                            <img class="sidebar-img" src="../../../../../views/images/components/informacoes-img.svg" alt="">
-                            <p class="sidebar-option normal-18-bold-title-2">Informações</p>
-                        </a>
-                    </li>
-
-                    <li class="sidebar-li">
                         <a href="../../message/list-message.page.php" class="sidebar-a-items">
                             <img class="sidebar-img" src="../../../../../views/images/components/fale-conosco-img.svg" alt="">
                             <p class="sidebar-option normal-18-bold-title-2">Fale Conosco</p>
