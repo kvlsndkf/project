14a266c2fbe99bfb7c5b8d7991f411b77ea9f7bf
Tiago Alves <tchaguwu@gmail.com>
2022-06-22 23:48:42 -0300

    fix: filled following

--- private/student/pages/feed-following/feed-following.page.php
+++ private/student/pages/feed-following/feed-following.page.php
@@ -93,7 +93,7 @@ try {
 
                     <li class="sidebar-li leftbar-li">
                         <a href="../feed-following/feed-following.page.php?userID=<?php echo $idUser; ?>" class="sidebar-a-items leftbar-a">
-                            <img class="leftbar-icon" src="../../../../views/images/components/following-icon.svg" alt="">
+                            <img class="leftbar-icon" src="../../../../views/images/components/filled-following.svg" alt="">
                             <p class="leftbar-text normal-18-bold-title-2">Seguindo</p>
                         </a>
                     </li>
