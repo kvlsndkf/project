5b5fc683184cf0eb545fa68a52b34d46773652af
Paloma <palomatj.gomes5@gmail.com>
2022-06-23 01:11:04 -0300

    feat/calma-respira

--- private/student/pages/detail-perfil-student/update-perfil-student.page.php
+++ private/student/pages/detail-perfil-student/update-perfil-student.page.php
@@ -29,6 +29,9 @@ try {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
+    <!-- Icon -->
+    <link rel="shortcut icon" href="../../../../views/images/favicon/favicon-16x16.png" type="image/x-icon">[]
+
     <!-- Link dos estilos gerais -->
     <link rel="stylesheet" href="../../../../views/styles/colors.style.css">
     <link rel="stylesheet" href="../../../../views/styles/font-format.style.css">
