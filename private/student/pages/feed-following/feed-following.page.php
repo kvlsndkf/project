33819a54f1a6c49f477bafccbebc95179126f299
Helena <hmiranda1818@gmail.com>
2022-06-23 22:23:18 -0300

    feat: ask button rule

--- private/student/pages/feed-following/feed-following.page.php
+++ private/student/pages/feed-following/feed-following.page.php
@@ -390,16 +390,28 @@ try {
                                     </p>
                                 </div>
 
-                                <a class="question-give-heelp-a pedir-heelp-button-a" href="../detail-question/detail-question.page.php?idQuestion=<?php echo $row->id; ?>">
-                                    <div class="question-toAnswer question-footer-div">
+                                <div class="<?php echo $styleDeleteQuestion; ?>">
+                                    <a class="question-give-heelp-a pedir-heelp-button-a" href="../detail-question/detail-question.page.php?idQuestion=<?php echo $row->id; ?>">
+                                        <div class="question-toAnswer question-footer-div">
 
-                                        <p class="normal-14-bold-p question-p white-text">Dar um help</p>
-                                        <img src="../../../../views/images/components/upper-line.svg" class="upper-line">
-                                        <img src="../../../../views/images/components/star-icon.svg" class="xp-star">
-                                        <p class="normal-14-bold-p question-p yellow-text"> <?php echo $row->xp; ?> xp </p>
+                                            <p class="normal-14-bold-p question-p white-text">Ver</p>
 
-                                    </div>
-                                </a>
+                                        </div>
+                                    </a>
+                                </div>
+
+                                <div class="<?php echo $styleDenunciationQuestion; ?>">
+                                    <a class="question-give-heelp-a pedir-heelp-button-a" href="../detail-question/detail-question.page.php?idQuestion=<?php echo $row->id; ?>">
+                                        <div class="question-toAnswer question-footer-div">
+
+                                            <p class="normal-14-bold-p question-p white-text">Ver</p>
+                                            <img src="../../../../views/images/components/upper-line.svg" class="upper-line">
+                                            <img src="../../../../views/images/components/star-icon.svg" class="xp-star">
+                                            <p class="normal-14-bold-p question-p yellow-text"> <?php echo $row->xp; ?> xp </p>
+
+                                        </div>
+                                    </a>
+                                </div>
 
                             </div>
 
@@ -643,7 +655,7 @@ try {
             <a href="../home/home.page.php" class="bottombar-a">
                 <img src="../../../../views/images/components/dashboard-img.svg" alt="">
             </a>
-            <a href="./feed-following.page.php?userID=<?php echo $idUser;?>" class="bottombar-a">
+            <a href="./feed-following.page.php?userID=<?php echo $idUser; ?>" class="bottombar-a">
                 <img src="../../../../views/images/components/filled-following.svg" alt="">
             </a>
             <a href="#" class="bottombar-a">
