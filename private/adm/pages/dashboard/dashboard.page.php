f0c92e42e4f46b35949edb89b619292598aecd75
Helena <hmiranda1818@gmail.com>
2022-06-24 23:56:31 -0300

    feat: link for access page

--- private/adm/pages/dashboard/dashboard.page.php
+++ private/adm/pages/dashboard/dashboard.page.php
@@ -113,7 +113,7 @@ try {
 
                 <li class="sidebar-li">
                     <p class="sidebar-categoria normal-14-bold-p">Contas</p>
-                    <a href="#" class="sidebar-a">
+                    <a href="../list-profiles/list-profiles.page.php" class="sidebar-a">
                         <img class="sidebar-img" src="../../../../views/images/components/listagem-img.svg" alt="">
                         <p class="sidebar-option normal-18-bold-title-2">Listagem</p>
                     </a>
