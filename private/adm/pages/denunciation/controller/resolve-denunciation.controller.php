d9bbeeee3337a15771061f0fa1f0f0d56340b0b6
Helena <hmiranda1818@gmail.com>
2022-06-27 22:25:04 -0300

    feat: controller denunciation

--- private/adm/pages/denunciation/controller/resolve-denunciation.controller.php
+++ private/adm/pages/denunciation/controller/resolve-denunciation.controller.php
@@ -4,7 +4,7 @@ require_once('/xampp/htdocs' . '/project/classes/denunciations/denunciation.clas
 
 session_start();
 
-if (isset($_POST['resolveDenunciation'])) {
+
     $denunciation = new Denunciation();
 
     $id = $_GET['denunciationID'];
@@ -13,9 +13,12 @@ if (isset($_POST['resolveDenunciation'])) {
     $answerID = $_GET['answerId'] ?? '';
     $userID = $_GET['denounced'] ?? '';
 
+    echo json_encode($_POST['context']) . "<br> <br>";
+    echo json_encode($_POST['context1']);
+
+
     $denunciation->setStatus("Resolvida");
     $denunciation->setContext($_POST['context']);
     $denunciation->setConclusion($_POST['conclusion']);
     $denunciation->setType($type);
-    $denunciation->moveResolved($denunciation, $id, $questionID, $answerID, $userID);
-}
+    // $denunciation->moveResolved($denunciation, $id, $questionID, $answerID, $userID);
